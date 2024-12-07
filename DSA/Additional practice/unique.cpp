18284b5381b21ea30589975e444acb1e262728ca
Anuj <anuj.23513@sscbs.du.ac.in>
2024-12-07 17:38:30 +0530

    Gcd Question using loops

---
+++ DSA/Additional practice/unique.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool unique(int arr,int n){
+    sort(arr);
+    for (int i=0;arr[i]==a[i-1];i++){
+        cout << arr[i]
+    }
+}
